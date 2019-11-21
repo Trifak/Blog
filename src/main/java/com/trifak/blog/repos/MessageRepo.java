51c385977ebcb1ef1f39ebe460388b454af70944
Ilya Trifonov <trifakk@gmail.com>
2019-11-21 13:08:56 +0300

    Add and connect DB (PostgreSQL) with Spring Boot JPA. Create a field to add messages and tags. Create a field to filter messages by tag.

---
+++ src/main/java/com/trifak/blog/repos/MessageRepo.java
@@ -0,0 +1,10 @@
+package com.trifak.blog.repos;
+
+import com.trifak.blog.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
+}
